e48ab4f8aa96f6d593edaec915518e57fc142c44
Tendani Netshitenzhe <ntsten002@myuct.ac.za>
2017-06-11 07:08:20 +0200

    Added Wishlist and Nanny Page

---
+++ Nation1/Models/NannyContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace Nation1.Models
+{
+    public class NannyContext : DbContext
+    {
+        public NannyContext() : base("Nation1")
+        {
+        }
+        public DbSet<Nanny> Nannies { get; set; }
+    }
+}
